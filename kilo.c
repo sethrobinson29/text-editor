3e55e0e1642f42b96dc14d783e8ab9f7d9531d98
Seth <sethpatrickrobinson@gmail.com>
2026-03-26 13:14:56 -0500

    fix if statement

--- kilo.c
+++ kilo.c
@@ -229,36 +229,28 @@ int is_separator(int c) {
 void editorUpdateSyntax(erow *row) {
 	row->hl = realloc(row->hl, row->rsize);
 	memset(row->hl, HL_NORMAL, row->rsize);
-
 	if (E.syntax == NULL) return;
-
 	char **keywords = E.syntax->keywords;
-
 	char *scs = E.syntax->singleline_comment_start;
 	char *mcs = E.syntax->multiline_comment_start;
 	char *mce = E.syntax->multiline_comment_end;
-
 	int scs_len = scs ? strlen(scs) : 0;
 	int mcs_len = mcs ? strlen(mcs) : 0;
 	int mce_len = mce ? strlen(mce) : 0;
-
 	int prev_sep = 1;
 	int in_string = 0;
 	int in_comment = (row->idx > 0 && E.row[row->idx - 1].hl_open_comment);
-
 	int i = 0;
 	while (i < row->rsize) {
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
-
-		if (scs_len && !in_string) {
+		if (scs_len && !in_string && !in_comment) {
 			if (!strncmp(&row->render[i], scs, scs_len)) {
 				memset(&row->hl[i], HL_COMMENT, row->rsize - i);
 				break;
 			}
 		}
-
-		if (mcs_len && mce_len && !in_string && !in_comment) {
+		if (mcs_len && mce_len && !in_string) {
 			if (in_comment) {
 				row->hl[i] = HL_MLCOMMENT;
 				if (!strncmp(&row->render[i], mce, mce_len)) {
@@ -278,7 +270,6 @@ void editorUpdateSyntax(erow *row) {
 				continue;
 			}
 		}
-
 		if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
 			if (in_string) {
 				row->hl[i] = HL_STRING;
@@ -300,7 +291,6 @@ void editorUpdateSyntax(erow *row) {
 				}
 			}
 		}
-
 		if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
 			if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
 				(c == '.' && prev_hl == HL_NUMBER)) {
@@ -310,7 +300,6 @@ void editorUpdateSyntax(erow *row) {
 				continue;
 			}
 		}
-
 		if (prev_sep) {
 			int j;
 			for (j = 0; keywords[j]; j++) {
@@ -324,13 +313,11 @@ void editorUpdateSyntax(erow *row) {
 					break;
 				}
 			}
-
 			if (keywords[j] != NULL) {
 				prev_sep = 0;
 				continue;
 			}
 		}
-
 		prev_sep = is_separator(c);
 		i++;
 	}
